e6d5c9b45d5dc70fad53202839c1aa1ef240065e
andrusha10t500 <andrusha10t500@gmail.com>
2019-05-12 18:01:16 +1000

    std

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ int main(int argc,char* args[])
     const int buf_size = 15638;
  
     // Имя исходного файла   
-    std::string src_file = args[1];
-    std::string out_file = args[2];
+    string src_file = args[1];
+    string out_file = args[2];
     cout << src_file << " -> " << out_file << "\n";
     
     // Создание потока для работы с исходным файлом
-    std::ifstream ifs(src_file.c_str(), std::ios::binary);
+    ifstream ifs(src_file.c_str(), ios::binary);
  
     // Подсчет размер исходного файла
-    ifs.seekg(0, std::ios::end);
-    std::ios::pos_type src_size = ifs.tellg();
-    ifs.seekg(0, std::ios::beg);
+    ifs.seekg(0, ios::end);
+    ios::pos_type src_size = ifs.tellg();
+    ifs.seekg(0, ios::beg);
     
     // Количество целых проходов (для частей файлы, которые полностью умещаются в буфер)
     size_t a_mount = src_size / buf_size;
@@ -28,7 +28,7 @@ int main(int argc,char* args[])
  
     
     // Создание потока для файла-копии
-    std::ofstream ofs(out_file.c_str(), std::ios::binary);
+    ofstream ofs(out_file.c_str(), ios::binary);
     
     // Это буфер
     char buf[buf_size];
